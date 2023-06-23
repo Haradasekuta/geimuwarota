e0a6595d5189f997dcc663290cdac68412120b80
minegishi <>
2023-06-23 12:26:13 +0900

    ゴール作成

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoalScript : MonoBehaviour
+{
+    Text text; // UIのテキストを入れる変数
+
+    void Start()
+    {
+        this.text = GameObject.Find("Text").GetComponent<Text>(); // テキストのオブジェクト名"Text"
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        text.text="ゲームクリア";
+        Debug.Log("ゲームクリア！");
+    }
+}
