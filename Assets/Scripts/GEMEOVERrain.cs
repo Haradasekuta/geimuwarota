0d7e06011abc7fd1b8422cae3702b99d76fcf86c
minegishi <>
2023-06-30 11:47:46 +0900

    ゲームオーバーの判定追加

---
+++ Assets/Scripts/GEMEOVERrain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GEMEOVERrain : MonoBehaviour
+{
+    private float topBoundary = -100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < topBoundary)
+        {
+            Debug.Log("Geme Over!");
+        }
+    }
+}
